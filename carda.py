0daa3c96846802b0a7d3e0453d7e851f13dab72c
Gandrillon Olivier <ogandril@sidus11>
2024-01-24 09:38:22 +0100

    carda

--- carda.py
+++ carda.py
@@ -9,7 +9,7 @@ from harissa.utils import build_pos, plot_network
 
 # Hyperparameters
 D=3461 # project name
-P=7 # Experiment within project
+P=9 # Experiment within project
 SFT=12 # time scale factor
 CC=12 # cell cycle time:
 T=6 #threshold for interactions 
@@ -19,9 +19,9 @@ sf=10 # scaling factor
 cwd = os.getcwd()
 
 Infer=1# to infer the GRN
-Simulate=1 # to simulate the GRN
-Visualize=1 # to visualize some output
-Kanto=1 # to compute Kantorovich distances
+Simulate=0 # to simulate the GRN
+Visualize=0 # to visualize some output
+Kanto=0 # to compute Kantorovich distances
 Draw=1 #to draw the GRN
 
 # Create a working directory
@@ -42,21 +42,24 @@ os.system("mkdir Rates")
 os.system("mkdir cardamom")
 
 # Launch R script to generate entry files
-os.system("Rscript --vanilla  "+str(cwd)+"/res_carda/3461_4.R "+str(SFT)+" "+str(CC)+" "+str(P)+" "+str(D))
+os.system("Rscript --vanilla  "+str(cwd)+"/res_carda/3461_3.R "+str(SFT)+" "+str(CC)+" "+str(P)+" "+str(D))
 
 # Infer GRN
 os.chdir(str(cwd)+"/OG"+str(D))
 if Infer:
 	os.system( "python infer_network.py -i " +str(P))
-
-os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/cardamom")
-fi=np.load('inter.npy')
-fi=fi*sf
-# Cut off low intensity edges
-fi[abs(fi) < T] = 0
-# Save the resulting matrix
-np.save('inter.npy', fi)
-np.savetxt("inter_matrix.csv", fi, delimiter=",")
+	os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/cardamom")
+	fi=np.load('inter.npy')
+	fi_t=np.load('inter_t.npy')
+	fi=fi*sf
+	fi_t=fi_t*sf
+	# Cut off low intensity edges
+	fi[abs(fi) < T] = 0
+	fi_t[abs(fi_t) < T] = 0
+	# Save the resulting matrix
+	np.save('inter.npy', fi)
+	np.save('inter_t.npy', fi_t)
+	np.savetxt("inter_matrix.csv", fi, delimiter=",")
 
 # Simulate
 os.chdir(str(cwd)+"/OG"+str(D))
