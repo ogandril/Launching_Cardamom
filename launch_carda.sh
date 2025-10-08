b6dbcd7332183f45057ff1e5c36dfd107a23a425
Olivier Gandrillon <ogandril@cca016.in2p3.fr>
2025-10-08 07:35:10 +0200

    launch

--- launch_carda.sh
+++ launch_carda.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#
 #SBATCH --job-name=3604
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=olivier.gandrillon@ens-lyon.fr
-#SBATCH --export=ALL
+#SBATCH --export=NONE
 source carda/bin/activate
 module load Programming_Languages/R/latest
+export PYTHONPATH="$(dirname $(which python))/../lib/python3.9/site-packages:$PYTHONPATH"
 python3 carda3.py
+
