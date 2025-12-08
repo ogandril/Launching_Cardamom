25c3f7baa627e7b5921f9c337a217542017e7558
ogandril <Olivier.Gandrillon@ens-lyon.fr>
2025-12-08 09:24:25 +0100

    carda2 3634

--- launch_carda2.sh
+++ launch_carda2.sh
@@ -3,8 +3,9 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=olivier.gandrillon@ens-lyon.fr
 #SBATCH --export=NONE
+module load Programming_Languages/python/3.12.2
 source carda_venv_12/bin/activate
 module load Programming_Languages/R/latest
 export PYTHONPATH="$(dirname $(which python))/../lib/python3.9/site-packages:$PYTHONPATH"
-/pbs/home/o/ogandril/carda_venv_12/bin/python /pbs/home/o/ogandril/Launching_Cardamom/carda2.py
+python /pbs/home/o/ogandril/Launching_Cardamom/carda2.py
 
