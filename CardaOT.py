4711bf2d09d4cbe0e01e77dc03d6a3d362c2e57e
ogandril <Olivier.Gandrillon@ens-lyon.fr>
2026-06-23 11:05:28 +0200

    Force_genes

--- CardaOT.py
+++ CardaOT.py
@@ -13,7 +13,7 @@ import sys
 cwd = os.getcwd()
 
 D=3747# project name
-P=1 #Experiment within project
+P=2 #Experiment within project
 Th_int=2 #threshold for interactions 
 
 # Which function should be executed
