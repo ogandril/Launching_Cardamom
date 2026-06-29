ac041ee80e47a9ea91ea745fdbdee15af77d14ba
ogandril <Olivier.Gandrillon@ens-lyon.fr>
2026-06-29 09:10:29 +0200

    3747

--- CardaOT.py
+++ CardaOT.py
@@ -156,7 +156,7 @@ if perturb:
 	os.system(f"python -m CardamomOT.cli step simulate_network -i {cwd}/OG{D}/{P}  -s full")
 	
 	os.system("echo 'Final checks_1'")
-	os.system(f"python -m CardamomOT.cli step check_sim_to_data -i {cwd}/OG{D}/{P} --stimulus 0.22-s full")
+	os.system(f"python -m CardamomOT.cli step check_sim_to_data -i {cwd}/OG{D}/{P} --stimulus 0.22 -s full")
 
 	os.system("echo 'Final checks_2'")
 	os.system(f"python -m CardamomOT.cli step simulate_network_KOV -i {cwd}/OG{D}/{P} -s full")
