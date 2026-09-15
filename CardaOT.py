430321f93fd06625ef5eff009c61e9b85c32e05d
ogandril <Olivier.Gandrillon@ens-lyon.fr>
2026-09-15 16:19:18 +0200

    3798

--- CardaOT.py
+++ CardaOT.py
@@ -59,7 +59,7 @@ if transform:
 	
 if Pre_comp:
 	os.system(f"cp  {cwd}/res_carda/data.h5ad "+path_6)
-	os.system(f"cp  {cwd}/res_carda/inter_ref.csv "+path_6)
+	#os.system(f"cp  {cwd}/res_carda/inter_ref.csv "+path_6)
 	# If uncorrected
 	os.system(f"cp  {cwd}/res_carda/data.h5ad {cwd}/res_carda/data_train.h5ad ")
 	os.system(f"cp  {cwd}/res_carda/data_train.h5ad "+path_6)
