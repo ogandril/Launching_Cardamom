4ddde85859ffaaa074ab4a0e62fac2fdf2c99f30
ogandril <Olivier.Gandrillon@ens-lyon.fr>
2026-09-15 15:23:55 +0200

    3798

--- CardaOT.py
+++ CardaOT.py
@@ -12,14 +12,14 @@ import sys
 # Pathways and files
 cwd = os.getcwd()
 
-D=3757# project name
-P=3 #Experiment within project
+D=3798# project name
+P=1 #Experiment within project
 
 # Which function should be executed
 transform=0 # old to new
 Pre_comp=1 # If a precomputed anndata is available
 Infer=1# to infer the GRN
-simulate=0# to simulate the GRN
+simulate=1# to simulate the GRN
 perturb=1# to perturb the GRN (KO/OV)
 
 # Create a working directory
